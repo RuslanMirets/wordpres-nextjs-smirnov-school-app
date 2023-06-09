f15b74ec23526286f6c5c999d7f2751fcc463aa6
Ruslan <ruslan.mirets@gmail.com>
2023-06-09 10:05:27 +0300

    Add NEXTAUTH_URL env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
 	env: {
 		WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
